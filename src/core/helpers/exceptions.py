b0efeac4b033c730c79206fec3eb9350c296783e
FusionSid <siddheshadsv@icloud.com>
2023-02-03 22:08:06 +1300

    feat: Add more exceptions for signing up

--- src/core/helpers/exceptions.py
+++ src/core/helpers/exceptions.py
@@ -71,6 +71,38 @@ class InvalidUsernameError(HTTPException):
         super().__init__(status_code, detail)
 
 
+class SignupConflictError(HTTPException):
+    def __init__(self) -> None:
+        status_code = 409
+        detail = (
+            {
+                "success": False,
+                "detail": "One of the values failed the unique check",
+                "tip": "Change some of the values and retry",
+            },
+        )
+        super().__init__(status_code, detail)
+
+
+class InputTooLong(HTTPException):
+    def __init__(self) -> None:
+        status_code = 409
+        detail = (
+            {
+                "success": False,
+                "detail": "One of the values given was too long",
+                "tip": "Try changing the length of the provided values to meet the length limits",
+                "limits": {
+                    "username": "Max 32 characters",
+                    "firstname": "Max 64 characters",
+                    "lastname": "Max 64 characters",
+                    "email": "Max 256 characters",
+                },
+            },
+        )
+        super().__init__(status_code, detail)
+
+
 class InvalidEmailError(HTTPException):
     def __init__(self, email: str) -> None:
         status_code = 422
