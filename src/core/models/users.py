d25eba6e7ee094b1af9bca1d72ab0fef01c4cdcc
FusionSid <siddheshadsv@icloud.com>
2023-02-03 22:07:13 +1300

    feat: create User model to represent a user

--- src/core/models/users.py
+++ src/core/models/users.py
@@ -2,14 +2,61 @@
 Contains models for users
 """
 
-__all__ = ["NewUserForm"]
+__all__ = ["NewUserForm", "User"]
 
 import re
 from typing import Optional
 
+from sqlalchemy import Column
+from sqlalchemy.dialects.postgresql import (
+    ARRAY,
+    BOOLEAN,
+    VARCHAR,
+    TEXT,
+    TIMESTAMP,
+    BIGINT,
+)
 from pydantic import BaseModel, EmailStr, EmailError, validator
 
+from core.db import Base
 from core import InvalidUsernameError, InvalidEmailError, argon2_hash
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    """
+    Base model for a user from the database
+
+    Attributes:
+        user_id (str): unique id to identify user, no other users can have that id.
+        username (str): the users Username. used to signup for the app and shown everywhere (unless display name)
+        password (str): the users hashed password
+        email (str): the users email
+        firstname (str): the users firstname
+        lastname (Optional[str]): the users lastname. This is optional
+        created_at (int): timestamp of when the user created their account
+        verified (bool): indicator of if the user has verified their email
+            and completed the signup process (default=false)
+        avatar (Optional[str]): link to the users profile picture
+            (or foreign key (uuid) to the user avatar table where the pfp is stored in bytes)
+        rooms (Optional[list[str]]): A list of rooms the user is the owner/admin of.
+            This list is a list of room ids aka foreign keys to the rooms table
+        display_name (Optional[str]): The users chosen display name.
+            By default this is none but if a user sets it they are displayed with that name
+    """
+
+    id = Column(BIGINT, primary_key=True, autoincrement=False)
+    username = Column(VARCHAR(32), unique=True, nullable=False)
+    password = Column(TEXT, nullable=False)
+    email = Column(VARCHAR(256), unique=True, nullable=False)
+    firstname = Column(VARCHAR(64), nullable=False)
+    created_at = Column(TIMESTAMP(timezone=False), nullable=False)
+    verified = Column(BOOLEAN, default=False)
+    lastname = Column(VARCHAR(64))
+    avatar = Column(TEXT)
+    rooms = Column(ARRAY(BIGINT))
+    display_name = Column(TEXT)
 
 
 class NewUserForm(BaseModel):
