174b92b4e159becf2bf897d4e67e5608bfe993b6
FusionSid <siddheshadsv@icloud.com>
2023-02-07 17:40:47 +1300

    update: include new models in models init file

--- src/core/models/__init__.py
+++ src/core/models/__init__.py
@@ -2,7 +2,23 @@
 The models directory is a module that contains classes
 """
 
-__all__ = ["ChatAPI", "NewUserForm", "limiter", "User", "user_pyd"]
+__all__ = (
+    "ChatAPI",
+    "NewUserForm",
+    "limiter",
+    "User",
+    "user_pyd",
+    "UserCache",
+    "BlacklistedEmail",
+    "BlacklistedIP",
+)
 
 from .chatapp import ChatAPI, limiter
-from .users import NewUserForm, User, user_pyd
+from .users import (
+    NewUserForm,
+    User,
+    user_pyd,
+    UserCache,
+    BlacklistedEmail,
+    BlacklistedIP,
+)
