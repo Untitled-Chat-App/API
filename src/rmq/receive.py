3be845b45323b06a72b1f14cea0329479270d219
FusionSid <siddheshadsv@icloud.com>
2023-03-09 18:06:54 +1300

    feat: add channel for sendings verification emails + channel for deleting user accounts

--- src/rmq/receive.py
+++ src/rmq/receive.py
@@ -1,31 +1,67 @@
-# these 2 are test queue callbacks
-# ill change them to actual functions later
+import os
 import json
 import asyncio
 
-from aio_pika import connect
+from tortoise import Tortoise
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+import aiosmtplib
+from dotenv import load_dotenv
+from aio_pika import connect, Message
 from aio_pika.abc import AbstractIncomingMessage
 
-from core import argon2_hash
+from core import RMQ_CONN_URL, User, TORTOISE_CONFIG
+
+load_dotenv()  # just incase ya feel me?
 
 
-async def on_message_email(message: AbstractIncomingMessage) -> None:
-    message_data = json.loads(message.body)
-    print(f"SENDING EMAIL TO {message_data['email']}")
+async def sendmail(message: MIMEMultipart):
+    smtp_client = aiosmtplib.SMTP(hostname="smtp.gmail.com", port=465, use_tls=True)
+
+    await smtp_client.connect()
+    await smtp_client.ehlo()
+    await smtp_client.login(os.environ["EMAIL"], os.environ["EMAIL_PASSWORD"])
+
+    try:
+        await smtp_client.send_message(message)
+    except (aiosmtplib.SMTPRecipientsRefused, aiosmtplib.SMTPResponseException):
+        data = {"user_id": message["user_id"]}
+        await delete_user_account(Message(json.dumps(data).encode()))
+
+    await smtp_client.quit()
 
 
-async def on_message_hash(message: AbstractIncomingMessage) -> None:
-    msg = message.body
-    msg = json.loads(msg)
-    print(await argon2_hash(msg["password"]))
+async def send_verification_email(msg: AbstractIncomingMessage) -> None:
+    message = json.loads(msg.body)
+
+    # Email data
+    email = MIMEMultipart("alternative")
+    email["To"] = message["email"]
+    email["From"] = "chat@fusionsid.xyz"
+    email["Subject"] = "Verify Untitled Chat Email!"
+    html = f"<h1>Token: {message['token']}</h1>"
+    email.attach(MIMEText(html, "html"))
+
+    await sendmail(email)
+
+
+async def delete_user_account(msg: AbstractIncomingMessage | Message) -> None:
+    message = json.loads(msg.body)
+    user_id = message["user_id"]
+
+    await User.filter(id=user_id).delete()
 
 
 async def rabbitmq_server() -> None:
+    # create db connection
+    await Tortoise.init(config=TORTOISE_CONFIG)
+
     CHANNELS = [
-        {"name": "send_emails", "callback": on_message_email},
-        {"name": "hash_pass", "callback": on_message_hash},
+        {"name": "verification_email", "callback": send_verification_email},
+        {"name": "delete_account", "callback": delete_user_account},
     ]
-    connection = await connect("amqp://guest:guest@localhost/")
+    connection = await connect(RMQ_CONN_URL)
     async with connection:
         for channel_data in CHANNELS:
             channel = await connection.channel()
