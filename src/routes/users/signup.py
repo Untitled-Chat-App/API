0cbffbe4c8bdb650f95854909b7bd75a6beecb52
FusionSid <siddheshadsv@icloud.com>
2023-03-09 18:10:42 +1300

    feat: complete user signup & verification endpoints

--- src/routes/users/signup.py
+++ src/routes/users/signup.py
@@ -7,15 +7,17 @@ __all__ = ["signup_endpoint"]
 from fastapi import APIRouter, Request
 from tortoise.exceptions import IntegrityError, ValidationError
 
+from rmq import send_to_channel
 from core import (
-    NewUserForm,
-    limiter,
-    generate_id,
     User,
-    SignupConflictError,
+    limiter,
+    user_cache,
+    generate_id,
+    NewUserForm,
     InputTooLong,
-    user_pyd,
+    SignupConflictError,
 )
+from core.helpers.tokens import create_access_token, check_valid_token
 
 signup_endpoint = APIRouter(
     tags=[
@@ -25,7 +27,7 @@ signup_endpoint = APIRouter(
 )
 
 
-@signup_endpoint.post("/signup")
+@signup_endpoint.post("/")
 @limiter.limit("1/hour")
 async def create_account(
     request: Request,
@@ -33,19 +35,43 @@ async def create_account(
 ):
     await new_user.hashpass()
 
-    # TODO check if ip ban
+    # check for conflilcting usernames or emails
+    if await User.exists(username=new_user.username):
+        raise SignupConflictError("username", new_user.username)
+    elif await User.exists(email=new_user.email):
+        raise SignupConflictError("email", new_user.email)
 
-    # TODO check for conflicts
+    user_id = generate_id("USER_ID")
 
     try:
-        user = await User.create(id=generate_id("USER_ID"), **new_user.dict())
-    except IntegrityError:
+        user = await User.create(id=user_id, **new_user.dict())
+    except IntegrityError:  # if there is a duplicate user
         raise SignupConflictError
-    except ValidationError:
+    except ValidationError:  # user data entered was too long for some of the inputs
         raise InputTooLong
 
-    return await user_pyd.from_tortoise_orm(user)
+    # generate verification token
+    token_id = generate_id("VERIF_TOK_ID")  # id is currently useless
+    token = await create_access_token(data={"user_id": user.id}, token_id=token_id)
 
-    # TODO send email
+    # send verification email to user
+    email_request_data = {"user_id": user.id, "email": user.email, "token": token}
+    await send_to_channel("verification_email", email_request_data)
 
-    # TODO response
+    # store user in cache
+    user_cache.set(user_id, user)
+
+    return {
+        "success": True,
+        "detail": "Verification email has been sent to provided email."
+        "Without verifying your account won't be created!",
+        "email_provided": user.email,
+        "if_no_email": "If you didn't recieve an email its because the email given was invalid."
+        "You will need to create a new account",
+    }
+
+
+@signup_endpoint.get("/verify")
+async def verify_user_account(request: Request, token: str):
+    await check_valid_token(token)
+    return {"success": True, "detail": "verified successfuly!"}
